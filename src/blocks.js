eb2f7ff6c0486c99c2599a9c122569910c429530
takky94 <infourbh@gmail.com>
2021-06-13 13:13:41 +0900

    テーブルのブロック追加

--- src/blocks.js
+++ src/blocks.js
@@ -1,6 +1,8 @@
 import "./table-title/block.js";
 import "./table-thick/block.js";
 import "./table-kg/block.js";
+import "./table-physical-property/block.js";
+import "./table-details/block.js";
 import "./case/block.js";
 import "./case-and-item/block.js";
 import "./remarks/block.js";
